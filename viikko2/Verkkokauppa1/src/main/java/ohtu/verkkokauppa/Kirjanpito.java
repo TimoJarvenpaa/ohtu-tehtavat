37266f896e3f9a2fc8f2a8dfc2dff03c6f4b9e67
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2019-11-07 16:18:16 +0200

    Tehtävä 10

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
@@ -4,20 +4,10 @@ package ohtu.verkkokauppa;
 import java.util.ArrayList;
 
 public class Kirjanpito implements KirjanpitoRajapinta {
-    private static Kirjanpito instance;
-    
-    public static Kirjanpito getInstance() {
-        if ( instance==null) {
-            instance = new Kirjanpito();
-        }
-        
-        return instance;
-    }
-    
     private ArrayList<String> tapahtumat;
 
-    private Kirjanpito() {
-        tapahtumat = new ArrayList<String>();
+    public Kirjanpito() {
+        this.tapahtumat = new ArrayList<String>();
     }
     
     @Override
