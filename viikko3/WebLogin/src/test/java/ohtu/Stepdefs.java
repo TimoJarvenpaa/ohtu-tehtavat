5ee6c8e099252d12da896e857e765efa0550c259
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2019-11-15 15:27:54 +0200

    Tehtävä 9

--- viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -36,7 +36,12 @@ public class Stepdefs {
     @When("correct username {string} and incorrect password {string} are given")
     public void correctUsernameAndIncorrectPasswordAreGiven(String username, String password) {
         logInWith(username, password);
-    }    
+    }
+    
+    @When("incorrect username {string} and password {string} are given")
+    public void incorrectUsernameAndPasswordAreGiven(String username, String password) {
+        logInWith(username, password);
+    }   
     
     @Then("user is not logged in and error message is given")
     public void userIsNotLoggedInAndErrorMessageIsGiven() {
@@ -44,10 +49,10 @@ public class Stepdefs {
         pageHasContent("Give your credentials to login");
     }    
     
-    @When("username {string} and password {string} are given")
-    public void usernameAndPasswordAreGiven(String username, String password) throws Throwable {
-        logInWith(username, password);
-    }   
+//    @When("username {string} and password {string} are given")
+//    public void usernameAndPasswordAreGiven(String username, String password) throws Throwable {
+//        logInWith(username, password);
+//    }   
     
     @Then("system will respond {string}")
     public void systemWillRespond(String pageContent) throws Throwable {
