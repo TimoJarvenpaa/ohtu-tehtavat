2e55754e87c1b1d9d547e958d10adf725a6b6c9c
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2019-11-21 14:52:39 +0200

    Tehtävä 4

--- viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -11,16 +11,23 @@ public class KauppaTest {
     Viitegeneraattori viite;
     Varasto varasto;
     Kauppa k;
+    String kaupanTili;
+    int viitenro;
 
     @Before
     public void setUp() {
         pankki = mock(Pankki.class);
         viite = mock(Viitegeneraattori.class);
         varasto = mock(Varasto.class);
+        kaupanTili = "33333-44455";
+        viitenro = 42;
         
         k = new Kauppa(varasto, pankki, viite);
         
-        when(viite.uusi()).thenReturn(42);
+        when(viite.uusi())
+                .thenReturn(viitenro)
+                .thenReturn(viitenro + 1)
+                .thenReturn(viitenro + 2);
         
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
@@ -30,56 +37,76 @@ public class KauppaTest {
         
         when(varasto.saldo(3)).thenReturn(0);
         when(varasto.haeTuote(3)).thenReturn(new Tuote(3, "viljaporsaan sisäfile", 7));
+        
+        k.aloitaAsiointi();
     }
 
     @Test
     public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
-        // tehdään ostokset
-        k.aloitaAsiointi();
-        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(1);
         k.tilimaksu("pekka", "12345");
 
-        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
         verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(), anyInt());
-        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
     }
 
     @Test
     public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaanOikeillaParametreilla() {
-        k.aloitaAsiointi();
         k.lisaaKoriin(1);
         k.tilimaksu("pekka", "12345");
 
-        verify(pankki).tilisiirto(eq("pekka"), anyInt(), eq("12345"), anyString(), eq(5));
+        verify(pankki).tilisiirto(eq("pekka"), eq(viitenro), eq("12345"), eq(kaupanTili), eq(5));
     }
 
     @Test
     public void kahdenEriTuotteenOstamisenLopuksiPankinMetodiaTilisiirtoKutsutaanOikeillaParametreilla() {
-        k.aloitaAsiointi();
         k.lisaaKoriin(1);
         k.lisaaKoriin(2);
         k.tilimaksu("pekka", "12345");
 
-        verify(pankki).tilisiirto(eq("pekka"), anyInt(), eq("12345"), anyString(), eq(6));
+        verify(pankki).tilisiirto(eq("pekka"), eq(viitenro), eq("12345"), eq(kaupanTili), eq(6));
     }
 
     @Test
     public void kahdenSamanTuotteenOstamisenLopuksiPankinMetodiaTilisiirtoKutsutaanOikeillaParametreilla() {
-        k.aloitaAsiointi();
         k.lisaaKoriin(1);
         k.lisaaKoriin(1);
         k.tilimaksu("pekka", "12345");
 
-        verify(pankki).tilisiirto(eq("pekka"), anyInt(), eq("12345"), anyString(), eq(10));
+        verify(pankki).tilisiirto(eq("pekka"), eq(viitenro), eq("12345"), eq(kaupanTili), eq(10));
     }
 
     @Test
     public void varastostaLoppuneenTuotteenOstamisenLopuksiPankinMetodiaTilisiirtoKutsutaanOikeillaParametreilla() {
-        k.aloitaAsiointi();
         k.lisaaKoriin(1);
         k.lisaaKoriin(3);
         k.tilimaksu("pekka", "12345");
 
-        verify(pankki).tilisiirto(eq("pekka"), anyInt(), eq("12345"), anyString(), eq(5));
+        verify(pankki).tilisiirto(eq("pekka"), eq(viitenro), eq("12345"), eq(kaupanTili), eq(5));
+    }
+    
+    @Test
+    public void kauppaPyytaaUudenViitenumeronJokaiselleMaksutapahtumalle() {
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+        
+        verify(viite, times(1)).uusi();
+        verify(pankki).tilisiirto(eq("pekka"), eq(viitenro), eq("12345"), eq(kaupanTili), eq(5));
+        
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);
+        k.tilimaksu("timo", "67890");
+        
+        verify(viite, times(2)).uusi();
+        verify(pankki).tilisiirto(eq("timo"), eq(viitenro + 1), eq("67890"), eq(kaupanTili), eq(1));
+    }
+    
+    @Test
+    public void tuotteenPoistaminenOstoskoristaOtetaanHuomioonOstoksiaMaksettaessa() {
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(2);
+        k.poistaKorista(2);
+        k.tilimaksu("pekka", "12345");
+        
+        verify(pankki).tilisiirto(eq("pekka"), eq(viitenro), eq("12345"), eq(kaupanTili), eq(5));
     }
 }
