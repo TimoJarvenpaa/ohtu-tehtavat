499e96a3f520b402a78d7a70f734bc95d9e0939a
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2019-12-06 12:50:16 +0200

    Tehtävä 4

---
+++ viikko6/QueryLanguage/src/main/java/statistics/matcher/QueryBuilder.java
@@ -0,0 +1,30 @@
+package statistics.matcher;
+
+public class QueryBuilder {
+    
+    private Matcher matcher;
+    
+    public QueryBuilder() {
+        this.matcher = new All();
+    }
+    
+    public Matcher build() {
+        return this.matcher;
+    }
+    
+    public QueryBuilder playsIn(String team) {
+        this.matcher = new And(new PlaysIn(team), this.matcher);
+        return this;
+    }
+    
+    public QueryBuilder hasAtLeast(int value, String category) {
+        this.matcher = new And(new HasAtLeast(value, category), this.matcher);
+        return this;
+    }
+    
+    public QueryBuilder hasFewerThan(int value, String category) {
+        this.matcher = new And(new HasFewerThan(value, category), this.matcher);
+        return this;
+    }
+    
+}
